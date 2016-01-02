6b20452cb5a51ed65bb14e566d4a94d1178a8c94
Jack McCracken <jackamc19@gmail.com>
2016-01-02 18:23:51 -0500

    Finish up Getting Started

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160102202255) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "commetor"
+    t.string   "commenter"
     t.text     "body"
     t.integer  "article_id"
     t.datetime "created_at", null: false
